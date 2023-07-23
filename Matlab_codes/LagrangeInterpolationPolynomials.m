2e1ddfb07ac37e8a2303d62fb4404ea1e4d51193
Obed Adu-Gyamfi <adugyamfiobed.ttp3@gmail.com>
2023-07-23 01:21:00 +0000

    Lagrange interpolation

--- Matlab_codes/LagrangeInterpolationPolynomials.m
+++ Matlab_codes/LagrangeInterpolationPolynomials.m
@@ -1,25 +1,65 @@
-x = [20 30 40 50];
-y = [512 390 360 243];
-xp = 30;
-n = length(x);
-sum = 0;
-sum1 = 0;
-product = 1;
-for i = 1 : n
-    for j = 1 : n
-        if i ~= j
-          fprintf('Iter i: %d \t Iter j: %d\n',i,j)
-          fprintf('********************************\n')
-          fprintf('xi: %d yi: %d \t xj:%d  yj:%d \n',x(i),y(i),x(j),y(j));
-          lasTerm = (xp - x(j)) /(x(i) - x(j));
-          product = lasTerm * product;
-          fprintf('_________________________________\n')
-        end      
-    end
-    sum = sum + y(i) * product;
-    %sum1 = sum1 + sum;
-    if j == n
-              product = 1;
-    end
+% Flowgorithm currently doesn't generate code for file IO.
+% Note: The first function listed is executed. main() is not a special function in Matlab/Octave.
+function main()
+    params = cell(2, 1);
+    
+    readParameters(params);
+    xvals = cell(params{index(0)}, 1);
+    yvals = cell(params{index(0)}, 1);
+    
+    readInterpData4x(xvals, params);
+    readinterpData4y(yvals, params);
+    interpolate(params, xvals, yvals);
 end
-fprintf('Yp: %d', sum);
+
+function interpolate(params, xvals, yvals)
+    sum = 0;
+    product = 1;
+    disp('Enter x to interpolate: ');
+    x = input('Enter a value for x');
+    for i = 0 : params{index(0)} - 1
+        for j = 0 : params{index(0)} - 1
+            if i ~= j
+                lasTerm = (x - xvals{index(j)}) / (xvals{index(i)} - xvals{index(j)});
+                product = lasTerm * product;
+            end
+        end
+        sum = sum + yvals{index(i)} * product;
+        if j == params{index(0)}
+            product = 1;
+        end
+    end
+    disp(['Sum: ', num2str(sum)]);
+end
+
+function readInterpData4x(xvals, params)
+    open "interpData4x.txt" to read
+    for j = 0 : params{index(0)} - 1
+        Read interpData4x
+        xvals{index(j)} = interpData4x;
+    end
+    close
+end
+
+function readinterpData4y(yvals, params)
+    open "interpData4y.txt" to read
+    for j = 0 : params{index(0)} - 1
+        Read interpData4y
+        yvals{index(j)} = interpData4y;
+    end
+    close
+end
+
+function readParameters(params)
+    open lagrParams.txt to read
+    for j = 0 : 1
+        Read lagrParams
+        params{index(j)} = lagrParams;
+    end
+    close
+end
+
+% Note: MATLAB uses 1-indexing for arrays. This function converts from zero-based to 1-based.
+function i = index(n)
+    i = n + 1;
+end
